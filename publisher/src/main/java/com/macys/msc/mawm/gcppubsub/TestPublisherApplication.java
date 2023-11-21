0f24c832aa503feec1d034f71789f697289139f8
Juan Haugaard <juan.haugaard@macys.com>
2023-11-21 11:48:48 -0500

    Update the library name

--- publisher/src/main/java/com/macys/msc/mawm/gcppubsub/TestPublisherApplication.java
+++ publisher/src/main/java/com/macys/msc/mawm/gcppubsub/TestPublisherApplication.java
@@ -3,7 +3,7 @@ package com.macys.msc.mawm.gcppubsub;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.macys.msc.mawm")
+@SpringBootApplication(scanBasePackages = "com.macys.msc.mawm.itemintegration.service,com.macys.msc.mawm.gcppubsub")
 public class TestPublisherApplication {
     public static void main(String[] args) {
         SpringApplication.run(TestPublisherApplication.class, args);
